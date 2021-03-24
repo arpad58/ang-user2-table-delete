96aa6d4d9d90682645c10de834d1ed1df2dd1b11
Arpad <solar@hu.inter.net>
2021-03-24 20:49:21 +0100

    delete works

--- src/app/page/user-list/user-list.component.ts
+++ src/app/page/user-list/user-list.component.ts
@@ -20,4 +20,14 @@ export class UserListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  /* delete */
+  onDelete(user: User): void {
+    alert('Biztos szeretné törölni?');
+    this.userService.delUser(user).subscribe(
+      () => {
+         this.users$ = this.userService.getAll();
+       }
+    );
+  }
+
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -16,7 +16,20 @@ export class UserService {
   ) { }
 
   /* table */
+  /**
+   * Get all users from the database.
+   * @returns on observable with all users.
+   */
   getAll(): Observable<User[]> {
     return this.http.get<User[]>(`${this.endpoint}`);
+  }
+
+  /**
+   * Delete a user from the database.
+   * The method is: this.http.delete
+   */
+   delUser(user: User): Observable<User> {
+    return this.http.delete<User>(`${this.endpoint}/${user.id}`);
+
   }
 }
